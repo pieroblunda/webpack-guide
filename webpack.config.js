dfe1a41556844118bb62ca0c4117520f12c9881f
pieroblunda <pieroblunda@gmail.com>
2019-05-19 12:40:01 +0200

    Loading fonts

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,11 @@ module.exports = {
         use: [
           'file-loader'
         ]
+      },{
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
       }
     ]
   }
